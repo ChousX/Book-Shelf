10432c7baa32bebb88d392129efa070ff1912bf5
ChousX <gmanenergy@gmail.com>
2022-11-21 13:03:55 -0500

    ahhhh

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
-use std::path::PathBuf;
+use book_shelf::*;
+fn main(){
+    let mut world = World::new();
 
-// use book_shelf::{file::Librarian, *};
-use chrono::Duration;
-fn main() {
-    // let mut book_shelf = BookShelf::default();
-    // let mut l = Librarian::new();
-    // l.add(r"V:\Local-Books").run(&mut book_shelf);
-    // for author in book_shelf.get_authors().into_iter() {
-        // println!("{author}");
-    // }
-    // if cfg!(cli) {
-        // println!("woop")
-    // }
 }
+
+fn spawn_book(world: &mut World){
+    
+}
+
